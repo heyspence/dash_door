d99ee65f059084531720cfe10a14c56c92e8d37e
“Spencer <Spencer@bookservo.com>
2023-11-06 20:19:59 -0500

    frontend config + migrations

---
+++ backend/db/migrate/20231107004836_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :phone_number, null: false, unique: true
+      t.string :email, null: false, unique: true
+      t.string :country, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false, unique: true
+
+      t.timestamps
+    end
+
+    add_index :users, :session_token, unique: true
+    add_index :users, :email, unique: true
+  end
+end
