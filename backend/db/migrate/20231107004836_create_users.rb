4b914bbae91797b19ee56a4dca49a3a46e8f221a
Spencer <spencer@bookservo.com>
2023-11-09 17:03:57 -0500

    user validations

--- backend/db/migrate/20231107004836_create_users.rb
+++ backend/db/migrate/20231107004836_create_users.rb
@@ -3,9 +3,9 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
-      t.string :phone_number, null: false, unique: true
+      t.string :phone_number, unique: true
       t.string :email, null: false, unique: true
-      t.string :country, null: false
+      t.string :country
       t.string :password_digest, null: false
       t.string :session_token, null: false, unique: true
 
