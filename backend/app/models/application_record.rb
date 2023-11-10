e5a786ac53a8a4204dcf0282a197916a44a86103
Spencer <spencer@bookservo.com>
2023-11-10 11:02:57 -0500

    backend user auth

--- backend/app/models/application_record.rb
+++ backend/app/models/application_record.rb
@@ -1,16 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-
-  def current_user
-    
-  end
-
-  def logged_in?
-  end
-
-  def login!(user)
-  end
-
-  def logout!
-  end
+  
 end
