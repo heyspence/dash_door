9f8ba6744b1deb46e2faa7f37ae5c4f8df794c80
“Spencer <Spencer@bookservo.com>
2023-11-11 11:25:47 -0500

    sign in modal

--- frontend/src/components/SplashPage/index.jsx
+++ frontend/src/components/SplashPage/index.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react';
+import splashPageImage from '../../assets/images/splash-main.webp'
 import './SplashPage.css'
+
+const backgroundStyle = {
+    backgroundImage: `url(${splashPageImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat'
+};
 
 const SplashPage = props => {
 
     return (
-        <div className='splash-page-main'>
+        <div className='splash-page-main' style={backgroundStyle}>
             <h2>Discover restaurants and more near you.</h2>
         </div>
     )
