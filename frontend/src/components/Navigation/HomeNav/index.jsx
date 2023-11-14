c6caa9e0f86fbd59ee83348f233b6e0bbaf356ac
Spencer <spencer@bookservo.com>
2023-11-13 20:52:08 -0500

    favicon issue

--- frontend/src/components/Navigation/HomeNav/index.jsx
+++ frontend/src/components/Navigation/HomeNav/index.jsx
@@ -11,8 +11,13 @@ const HomeNav = ({ display, toggleMenu, toggleCart }) => {
         <div className="home-nav">
             <div className="home-nav-left">
                 <HamburgerMenu onClick={toggleMenu}/>
-                <img src={logo} />
-                <h1>DASHDOOR</h1>
+                <div className="home-nav-left-inner-container">
+                    <div className="logo-container">
+                        <img src={logo} />
+                        <h1>DASHDOOR</h1>
+                    </div>
+                    <h3>Home</h3>
+                </div>
             </div>
             <div className="home-nav-right">
                 <CartIcon onClick={toggleCart} />
